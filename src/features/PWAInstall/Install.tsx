2c1b6aa15f7edb187c92cb03b6f2d79d71743da9
Arvin Xu <arvinx@foxmail.com>
2024-10-28 03:09:04 +0000

    FIX AGAIN

--- src/features/PWAInstall/Install.tsx
+++ src/features/PWAInstall/Install.tsx
@@ -12,7 +12,7 @@ import { systemStatusSelectors } from '@/store/global/selectors';
 import { useUserStore } from '@/store/user';
 
 // @ts-ignore
-const PWA: any = dynamic(() => import('@khmyznikov/pwa-install/dist/react-legacy/pwa-install.react-legacy.js'), {
+const PWA: any = dynamic(() => import('@khmyznikov/pwa-install'), {
   ssr: false,
 });
 
